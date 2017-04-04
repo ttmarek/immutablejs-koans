0b52581a9b8a0365bf81a6787b13dcf077f8cc2c
ttmarek <ttmarek@outlook.com>
2017-04-04 11:38:48 -0400

    Update List koans

--- __tests__/01-lists.js
+++ __tests__/01-lists.js
@@ -22,14 +22,7 @@ describe('List', () => {
     expect(nameList.toArray()).toEqual(['j', 'o', 'h', 'n']);
   });
 
-  test('creating a List by setting values', () => {
-    let table = Immutable.List();
-    table = table.set(0, 'forks');
-    table = table.set(1, 'plate');
-    table = table.set(2, 'knife');
-    table = table.set(3, 'spoon');
-    expect(table.toArray()).toEqual(['forks', 'plate', 'knife', 'spoon']);
-  });
+
 
   test('creating a List from nested lists', () => {
     // List only creates a List from a top level of an Array
@@ -68,5 +61,91 @@ describe('List.of', () => {
   });
 });
 
-describe('fromJS', () => {
+describe('List.set', () => {
+  test('creating a List by setting values', () => {
+    let table = Immutable.List();
+    table = table.set(0, 'forks');
+    table = table.set(1, 'plate');
+    table = table.set(2, 'knife');
+    table = table.set(3, 'spoon');
+    expect(table.toArray()).toEqual(['forks', 'plate', 'knife', 'spoon']);
+  });
+});
+
+describe('List.size', () => {
+  test('finding the size of a List', () => {
+    const fruits = ['apple', 'banana', 'pear'];
+    const vegetables = ['carrot', 'lettuce', 'kale', 'eggplant'];
+    const fruitsList = Immutable.List(fruits);
+    const vegetablesList = Immutable.List(vegetables);
+    expect(fruitsList.size).toBe(3);
+    expect(vegetablesList.size).toBe(4);
+  });
+});
+
+describe('List.delete', () => {
+  test('removing an item from a List', () => {
+    let fruitsList = Immutable.List(['apple', 'banana', 'pear']);
+    expect(fruitsList.size).toBe(3);
+    fruitsList = fruitsList.delete(1);
+    expect(fruitsList.size).toBe(2);
+    expect(fruitsList.toArray()).toEqual(['apple', 'pear']);
+  });
+  test('you can also use the alias List.remove', () => {
+    let vegetablesList = Immutable.List(['carrot', 'lettuce', 'beets']);
+    expect(vegetablesList.size).toBe(3);
+    vegetablesList = vegetablesList.remove(0);
+    expect(vegetablesList.size).toBe(2);
+    expect(vegetablesList.toArray()).toEqual(['lettuce', 'beets']);
+  });
+});
+
+describe('List.insert', () => {
+  test('adding an item to a List at a specific index', () => {
+    let fruitsList = Immutable.List(['apple', 'banana', 'pear']);
+    fruitsList = fruitsList.insert(1, 'pineapple');
+    expect(fruitsList.toArray()).toEqual(['apple', 'pineapple', 'banana', 'pear']);
+  });
+});
+
+describe('List.merge', () => {
+  test('merging a List with an array', () => {
+    let fruitsList = Immutable.List(['apple', 'banana', 'pear']);
+    fruitsList = fruitsList.merge(['pineapple', 'kiwi']);
+    expect(fruitsList.toArray()).toEqual(['pineapple', 'kiwi', 'pear']);
+  });
+  test('merging a List with another List', () => {
+    const fruitsList = Immutable.List(['apple', 'banana']);
+    const vegetablesList = Immutable.List(['carrot', '', 'lettuce', 'kale']);
+    const mergedList = vegetablesList.merge(fruitsList);
+    expect(mergedList.toArray()).toEqual(['apple', 'banana', 'lettuce', 'kale']);
+  });
+});
+
+describe('List.mergeWith', () => {
+  test('merging a List with an array', () => {
+    const fruits = Immutable.List.of('kiwi', 'banana', '');
+    const veg = Immutable.List.of('carrot', 'kale', 'lettuce');
+    const merger = (oldVal, newVal) => oldVal.length > newVal.length ? oldVal : newVal;
+    const mergedList = veg.mergeWith(merger, fruits);
+    expect(mergedList.toArray()).toEqual(['carrot', 'banana', 'lettuce']);
+  });
+});
+
+describe('List.mergeDeep', () => {
+  test('merging a deeply nested List with a deeply nested array', () => {
+    const fruits = Immutable.fromJS(
+      ['kiwi', ['pineapple', 'apple', ['banana', 'orange']]]
+    );
+    const vegies = Immutable.fromJS(
+      ['carrot', 'kale', ['lettuce', ['cabbage', 'tomato'], '']]
+    );
+    const mergedLists = vegies.mergeDeep(fruits);
+    expect(mergedLists.toJS()).toEqual(
+      [
+        'kiwi', ['pineapple', 'apple', ['banana', 'orange']],
+        ['lettuce', ['cabbage', 'tomato'], '']
+      ]
+    );
+  });
 });
